5a61f0b0516111e189953ff4b3f08f307eef4b41
aminulislam01101 <aminul.islam01101@gmail.com>
2022-12-25 16:17:22 +0600

    mongo db connection succesfull

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.use((req, res, next) => {
     next();
 });
 
+
 // Set up default mongoose connection
 const mongoDB = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dtbllhc.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -30,53 +31,61 @@ const client = new MongoClient(mongoDB, {
 });
 
 // JWT middleware
-function verifyJWT(req, res, next) {
-    const authHeader = req.headers.authorization;
-    console.log(authHeader.error);
+// function verifyJWT(req, res, next) {
+//     const authHeader = req.headers.authorization;
+//     console.log(authHeader.error);
 
-    if (!authHeader) {
-        return res.status(401).send('unauthorized access');
-    }
+//     if (!authHeader) {
+//         return res.status(401).send('unauthorized access');
+//     }
 
-    const token = authHeader.split(' ')[1];
-    console.log(token.warn);
+//     const token = authHeader.split(' ')[1];
+//     console.log(token.warn);
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(403).send({ message: 'forbidden access' });
-        }
-        req.decoded = decoded;
-        console.log(req.decoded);
-        next();
-    });
-}
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//         if (err) {
+//             return res.status(403).send({ message: 'forbidden access' });
+//         }
+//         req.decoded = decoded;
+//         console.log(req.decoded);
+//         next();
+//     });
+// }
 
 const run = async () => {
     try {
         const usersCollection = client.db('remoStart').collection('users');
 
         const blogsCollection = client.db('remoStart').collection('blogs');
-
+        const categoryCollection = client.db('remoStart').collection('blogCategory');
         // verify admin middleware
-        const verifyAdmin = async (req, res, next) => {
-            const decodedEmail = req.decoded.email;
-            console.log(decodedEmail.error);
+        // const verifyAdmin = async (req, res, next) => {
+        //     const decodedEmail = req.decoded.email;
+        //     console.log(decodedEmail.error);
 
-            const query = { email: decodedEmail };
-            const user = await usersCollection.findOne(query);
+        //     const query = { email: decodedEmail };
+        //     const user = await usersCollection.findOne(query);
 
-            if (user?.role !== 'admin') {
-                return res.status(403).send({ message: 'forbidden access' });
-            }
-            next();
-        };
+        //     if (user?.role !== 'admin') {
+        //         return res.status(403).send({ message: 'forbidden access' });
+        //     }
+        //     next();
+        // };
+        console.log('collectionH ',categoryCollection);
+        
 
         // GET BLOGS
         app.get('/blogs', async (req, res) => {
+            console.log('hello');
+            
             const filter = {};
             const result = await blogsCollection.find(filter).toArray();
+            console.log(result);
+            
             res.send(result);
         });
+
+
     } finally {
     }
 };
